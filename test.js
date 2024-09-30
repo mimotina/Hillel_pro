11569d51fe1fb76bc3ffde6f574c1bc9b587762c
vovnytska <iryna.vovnytska@wtt-solutions.com>
2024-10-01 00:09:17 +0200

    HW5-1

--- test.js
+++ test.js
@@ -1,27 +1,17 @@
 
-function checkProbabilityTheory(count) {
-  let parni =0
-  let neparni=0
-  let verojatnost
-  const min = 100;
-  const max = 1000; 
+var obj ={
+  name:"Jon",
+  sName:"Smith",
+  age:25, 
+    
+}
+function getInfo(){
+for (let key in obj){
+  console.log(key +': ' + obj[key])}
+}
+getInfo()
+obj.newProperty='Lviv'
+getInfo()
 
- let first = count
-for (let i = 0; i < first ; i++) {
-  const randomInRange = Math.floor((Math.random() * max) + min);
 
-if  ( randomInRange%2 ==0 ) parni=++parni
-else neparni = ++neparni
-
-verojatnost=parni/neparni*100
-  }
-  console.log("parni", parni)
-  console.log("neparni", neparni)
-  console.log("count", count)
-  return verojatnost
-} 
-
-let count = Math.floor(Math.random() * 1000)
-let Probability = checkProbabilityTheory(count) 
-console.log("Probability parno do neparni", Probability)
 
