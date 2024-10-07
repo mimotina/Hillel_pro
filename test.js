59f270305e3637adf34f699b1370914528bace46
Ирина Вовницкая <irinavovnitskaya@MacBook-Air-Irina.local>
2024-10-08 00:35:15 +0200

    HW-6

--- test.js
+++ test.js
@@ -1,17 +1,63 @@
+const company = {
+  name: 'Велика Компанія',
+  type:'Головна компанія',
+  platform: 'Платформа для продажу квитків',
+  sellsSolution: 'Рішення для продажу квитків',
+  clients: [
+    {
+      name: 'Клієнт 1',
+      type: 'subCompany',
+      uses: 'ПО для продажу квитків',
+      sells: 'Рішення для продажу квитків',
+      partners: [
+        {
+          name: 'Клієнт 1.1',
+          type: 'subSubCompany',
+          uses: 'Рішення для продажу квитків',
+          sells: 'Рішення для продажу квитків',
+        },
+        {
+          name: 'Клієнт 1.2',
+          type: 'subSubCompany',
+          uses: 'Рішення для продажу квитків',
+          sells: 'Рішення для продажу квитків',
+          partners: [
+            {
+              name: 'Клієнт 1.2.3',
+              type: 'subSubCompany',
+              uses: 'Рішення для продажу квитків',
+              sells: 'Рішення для продажу квитків',
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Клієнт 2',
+      type: 'subCompany',
+      uses: 'ПО для продажу квитків',
+      sells: 'Рішення для продажу квитків'
+    }
+  ]
+};
 
-var obj ={
-  name:"Jon",
-  sName:"Smith",
-  age:25, 
+function findValueByKey(companyName) {
     
-}
-function getInfo(){
-for (let key in obj){
-  console.log(key +': ' + obj[key])}
-}
-getInfo()
-obj.newProperty='Lviv'
-getInfo()
+  for (let i in company) {
+      if (company[i]===companyName){
+          return company
+      }
+  else {   
+      if(Array.isArray(company[i]) && company[i]!==companyName ){
+          findValueByKey(company[i]);
+           return company[i]
+      }        
+       
 
+  }
+}
+}
+
+  console.log(findValueByKey('Клієнт 2'))
 
 
