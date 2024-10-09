a76e6e5dd497cfdf1be2bd8fc35d353838135a34
vovnytska <iryna.vovnytska@wtt-solutions.com>
2024-10-09 22:05:49 +0200

    HW7

--- test.js
+++ test.js
@@ -1,63 +1,17 @@
-const company = {
-  name: 'Велика Компанія',
-  type:'Головна компанія',
-  platform: 'Платформа для продажу квитків',
-  sellsSolution: 'Рішення для продажу квитків',
-  clients: [
-    {
-      name: 'Клієнт 1',
-      type: 'subCompany',
-      uses: 'ПО для продажу квитків',
-      sells: 'Рішення для продажу квитків',
-      partners: [
-        {
-          name: 'Клієнт 1.1',
-          type: 'subSubCompany',
-          uses: 'Рішення для продажу квитків',
-          sells: 'Рішення для продажу квитків',
-        },
-        {
-          name: 'Клієнт 1.2',
-          type: 'subSubCompany',
-          uses: 'Рішення для продажу квитків',
-          sells: 'Рішення для продажу квитків',
-          partners: [
-            {
-              name: 'Клієнт 1.2.3',
-              type: 'subSubCompany',
-              uses: 'Рішення для продажу квитків',
-              sells: 'Рішення для продажу квитків',
-            }
-          ]
-        }
-      ]
-    },
-    {
-      name: 'Клієнт 2',
-      type: 'subCompany',
-      uses: 'ПО для продажу квитків',
-      sells: 'Рішення для продажу квитків'
-    }
-  ]
-};
-
-function findValueByKey(companyName) {
-    
-  for (let i in company) {
-      if (company[i]===companyName){
-          return company
-      }
-  else {   
-      if(Array.isArray(company[i]) && company[i]!==companyName ){
-          findValueByKey(company[i]);
-           return company[i]
-      }        
-       
-
-  }
-}
-}
-
-  console.log(findValueByKey('Клієнт 2'))
-
-
+var arr = [
+  {
+      userName:"Test",
+      lastName:"Test",
+      <email:"test.test@gmail.com>"
+  },
+  {
+      userName:"Dmitro",
+      lastName:"Porohov",
+      <email:"dmitro.porohov@yahoo.com>"
+  },
+  {
+      userName:"Andrii",
+      lastName:"",
+      <email:"andrii@mail.ru>" // Нам такі не підходять
+  },
+];
---
+++ test2.js
@@ -0,0 +1,9 @@
+var str = "Wonderful, Joyful, Happiness, Time, Task, Apple,";
+var pattern = / [b-zB-z]{6,}/g;
+//var word = str.split(', ');
+
+  
+console.log(str.match(pattern))
+//console.log(word)
+
+    
