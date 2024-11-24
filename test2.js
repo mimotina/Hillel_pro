c1bd90545f298efc69aef33d716feafd76a344f0
mimotina <mimotina2015@gmail.com>
2024-11-24 22:27:05 +0100

    hw7-transaction

--- test2.js
+++
@@ -1,9 +0,0 @@
-var str = "Wonderful, Joyful, Happiness, Time, Task, Apple,";
-var pattern = / [b-zB-z]{6,}/g;
-//var word = str.split(', ');
-
-  
-console.log(str.match(pattern))
-//console.log(word)
-
-    
